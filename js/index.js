1f2b03bd7bbab2dc58df2767ad4b0ad01b8b91c8
edvard01 <frejaclueless@gmail.com>
2024-01-29 14:49:31 +0100

    Started trying out different stuff with how to display the items

--- js/index.js
+++ js/index.js
@@ -11,11 +11,25 @@ const listArray = [];
 const btn = document.getElementById("submitListItem");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  let text = document.getElementById("description");
-  let author = document.getElementById("author");
+  let text = document.getElementById("description").value;
+  let author = document.getElementById("author").value;
   let id = listArray.length + 1;
 
   listArray.push(new ListObject(id, text, author));
+  updateList();
 });
 
-function updateList() {}
+function updateList() {
+  const list = document.querySelector(".todo-list");
+  console.log(list);
+  let html = "";
+  listArray.forEach((item) => {
+    console.log(item);
+    html += `<li><span class="left-section"><h2>${item.description}</h2>`;
+    html += `<h3>Author: ${item.author}</h3>`;
+    html += `<label>Done:</label>`;
+    html += `<input type="checkbox" id="${item.id}"></span>`;
+    html += `<span class="right-section"><h2 id="up">Move Up</h2><h2 id="down">Move Down</h2></span>`;
+  });
+  list.innerHTML = html;
+}
